04b87ae520fc3be277ce5f22488a91ee45f3dded
rojung <roland.jung@aau.at>
2018-06-08 11:14:34 +0200

    task: video_dlib supports webcam + width/height arguments + fps output

--- code/test_on_video_dlib.py
+++ code/test_on_video_dlib.py
@@ -16,6 +16,8 @@ from PIL import Image
 
 import datasets, hopenet, utils
 
+import time
+
 from skimage import io
 import dlib
 
@@ -28,16 +30,18 @@ def parse_args():
           default='', type=str)
     parser.add_argument('--face_model', dest='face_model', help='Path of DLIB face detection model.',
           default='', type=str)
-    parser.add_argument('--video', dest='video_path', help='Path of video')
+    parser.add_argument('--video', dest='video_path', help='Path of video; 0 or webcam or empty == webcam', default="")
     parser.add_argument('--output_string', dest='output_string', help='String appended to output file')
-    parser.add_argument('--n_frames', dest='n_frames', help='Number of frames', type=int)
+    parser.add_argument('--n_frames', dest='n_frames', help='Number of frames; 0==all', type=int)
     parser.add_argument('--fps', dest='fps', help='Frames per second of source video', type=float, default=30.)
+    parser.add_argument('--width', help="max width", type=int, default=480)
+    parser.add_argument('--height', help="max height", type=int, default=320)
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args = parse_args()
-
+    print(cv2.__version__)
     cudnn.enabled = True
 
     batch_size = 1
@@ -49,29 +53,26 @@ if __name__ == '__main__':
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    if not os.path.exists(args.video_path):
-        sys.exit('Video does not exist')
-
     # ResNet50 structure
     model = hopenet.Hopenet(torchvision.models.resnet.Bottleneck, [3, 4, 6, 3], 66)
 
     # Dlib face detection model
     cnn_face_detector = dlib.cnn_face_detection_model_v1(args.face_model)
 
-    print 'Loading snapshot.'
+    print ('Loading snapshot.')
     # Load snapshot
     saved_state_dict = torch.load(snapshot_path)
     model.load_state_dict(saved_state_dict)
 
-    print 'Loading data.'
+    print ('Loading data.')
 
-    transformations = transforms.Compose([transforms.Scale(224),
+    transformations = transforms.Compose([transforms.Resize(224),
     transforms.CenterCrop(224), transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
     model.cuda(gpu)
 
-    print 'Ready to test network.'
+    print ('Ready to test network.')
 
     # Test the Model
     model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
@@ -80,11 +81,22 @@ if __name__ == '__main__':
     idx_tensor = [idx for idx in xrange(66)]
     idx_tensor = torch.FloatTensor(idx_tensor).cuda(gpu)
 
-    video = cv2.VideoCapture(video_path)
+    using_webcam = False
+    print("video path: " + video_path)
+    if video_path == "0" or video_path == "" or video_path == "webcam":
+        print("using webcam...")
+        using_webcam = True
+        video = cv2.VideoCapture(0)
+    else:
+        video = cv2.VideoCapture(video_path)
 
     # New cv2
     width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))   # float
     height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)) # float
+    if height > args.height or width > args.width:
+        width = args.width
+        height = args.height
+
 
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
@@ -101,13 +113,25 @@ if __name__ == '__main__':
     txt_out = open('output/video/output-%s.txt' % args.output_string, 'w')
 
     frame_num = 1
+    total_time = 0
+    num_frames = args.n_frames
+    if num_frames < 1:
+        num_frames = 100000000
 
-    while frame_num <= args.n_frames:
-        print frame_num
+    ret = True
+    while frame_num <= num_frames and ret:
+        start_time = time.time()
 
         ret,frame = video.read()
-        if ret == False:
+        if not ret:
+            print("error...")
             break
+        height, width = frame.shape[:2]
+
+        if height > args.height or width > args.width:
+            frame = cv2.resize(frame, (args.width, args.height))
+            # height, width = frame.shape[:2]
+            # print ("dim: " + str(height) + "x" + str(width))
 
         cv2_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
@@ -156,9 +180,20 @@ if __name__ == '__main__':
                 # utils.plot_pose_cube(frame, yaw_predicted, pitch_predicted, roll_predicted, (x_min + x_max) / 2, (y_min + y_max) / 2, size = bbox_width)
                 utils.draw_axis(frame, yaw_predicted, pitch_predicted, roll_predicted, tdx = (x_min + x_max) / 2, tdy= (y_min + y_max) / 2, size = bbox_height/2)
                 # Plot expanded bounding box
-                # cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
+                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
 
         out.write(frame)
+        if using_webcam:
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        elapsed_time = time.time() - start_time
+        total_time += elapsed_time
+        print ("frame nr: " + str(frame_num) +
+               "; fps: " + str(format(1.0/elapsed_time, '.2f')) +
+               "; total: " + str(format(total_time, '.2f')))
+
         frame_num += 1
 
     out.release()
